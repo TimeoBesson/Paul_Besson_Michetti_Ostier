6332f30ab6439e94b58b52c384a771fef2472458
Matheis <matheis.gilbrin@etu.univ-smb.fr>
2026-05-26 14:22:03 +0200

    Ajoutez des fichiers projet.

---
+++ Paul_Besson_Michetti_Ostier/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Paul_Besson_Michetti_Ostier
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
