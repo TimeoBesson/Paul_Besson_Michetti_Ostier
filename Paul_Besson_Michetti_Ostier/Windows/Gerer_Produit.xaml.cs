749880f902eefc6c6c6169e3f215b1b565d4f052
CAMARAMohamed01 <mohamed.camarar@etu.univ-smb.fr>
2026-05-28 15:21:40 +0200

    debut page Gerer Produit

---
+++ Paul_Besson_Michetti_Ostier/Windows/Gerer_Produit.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Paul_Besson_Michetti_Ostier.Windows
+{
+    /// <summary>
+    /// Logique d'interaction pour Gerer_Produit.xaml
+    /// </summary>
+    public partial class Gerer_Produit : Window
+    {
+        public Gerer_Produit()
+        {
+            InitializeComponent();
+        }
+
+        private void TabControl_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
