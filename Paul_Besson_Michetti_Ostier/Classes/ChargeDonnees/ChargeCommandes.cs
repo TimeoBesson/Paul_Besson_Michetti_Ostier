b8e2ea6ea75314a9bac201eca3eab9c35a1d5adc
TiméoBesson <timeo.besson@etu.univ-smb.fr>
2026-06-02 09:43:36 +0200

    CHragement des commandes

--- Paul_Besson_Michetti_Ostier/Classes/ChargeDonnees/ChargeCommandes.cs
+++ Paul_Besson_Michetti_Ostier/Classes/ChargeDonnees/ChargeCommandes.cs
@@ -16,17 +16,12 @@ namespace Paul_Besson_Michetti_Ostier.Classes.ChargeDonnees
             try
             {
                 this.LesCommandes = new ObservableCollection<Commande>(new Commande().FindAll());
-                //foreach (Commande s in this.LesCommandes)
-                //{
-                //    s.UnClient = LesClients.FirstOrDefault(c => c.IdClient == s.IdClient);
-
-
-                //}
+                
             }
             catch (Exception ex)
             {
                 throw new Exception("Impossible de charger les données. Voir votre admin.", ex);
-                //    ErrorMessage = "Impossible de charger les données. Voir votre admin.";
+                
             }
 
 
